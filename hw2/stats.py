eb77c969de9e701855d537f9d2fb4ee0179199c3
DimiKon <dimkontoe@gmail.com>
2021-11-10 18:48:23 +0200

    assign 2

--- hw2/stats.py
+++ hw2/stats.py
@@ -24,7 +24,8 @@ for sil in reversed(all_silence):
     data.loc[sil-1][1]= data.loc[sil-1][1] + ", " + data.loc[sil][1]
 
 # total number of turns_bot
-bot_turns_total = len (index)
+print(f"Total number of bot's turns: {len (index)}.")
+
 
 data = data.drop(all_silence)
 
@@ -32,19 +33,20 @@ data = data.drop(all_silence)
 index = data.index
 
 # total number of turns_user
-user_turns_total = len(index)
+print(f"Total number of user's turns: {len (index)}.")
 
 data['tokenized_user'] = data.apply(lambda row: word_tokenize(row['user']), axis=1)
 data['word_counts_user'] = data.apply(lambda row: len(row['tokenized_user']), axis=1)
 
 #total number of words_user
-words_total_user = data['word_counts_user'].sum()
+print(f"Total number of user's words: {data['word_counts_user'].sum()}.")
+
 
 data['tokenized_bot'] = data.apply(lambda row: word_tokenize(row['bot']), axis=1)
 data['word_counts_bot'] = data.apply(lambda row: len(row['tokenized_bot']), axis=1)
 
 #total number of words_bot
-words_total_bot = data['word_counts_bot'].sum()        
+print(f"Total number of bot's words: {data['word_counts_bot'].sum()}.")
 
 
 dialogues_total = 0
@@ -55,31 +57,16 @@ for n in data.itertuples(name=None):
     counter+=1
     if n[3] == "1":
         turns_per_dialog.append(counter)
-        #words = n[5]
-        #words_per_dialog.append(words)
         dialogues_total+=1
         counter = 0
 
 turns_per_dialog.append(counter+1)
-turns_per_dialog = turns_per_dialog[1:]
+print(f"Turns per dialogue: {turns_per_dialog[1:]}.")
 
+print(f"Mean dialogue length: {statistics.mean(turns_per_dialog)}.")
+print(f"Std of dialogue lenghts: {statistics.stdev(turns_per_dialog)}.")
 
-for i in turns_per_dialog:
-    
-
-
-#for x in data.itertuples(name=None):
-    #print(x)
-
-
-
-#mean_num_turns = statistics.mean(turns_per_dialog)
-
-#std_turns = statistics.stdev(turns_per_dialog)
-
-
-#print("Mean of the sample is % s " %(statistics.mean(data))) 
-
-
-
+results = set()
+data['tokenized_all']= data['tokenized_user']+data['tokenized_bot']
+print(f"Vocabulary size: {len(data['tokenized_all'].apply(results.update))}.")
 
